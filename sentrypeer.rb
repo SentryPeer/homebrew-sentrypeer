311c85a92dc17992cecd8986066d0b044d2c3d46
Gavin Henry <ghenry@surevoip.co.uk>
2022-01-18 21:30:15 +0000

    v0.0.5 released.

--- sentrypeer.rb
+++ sentrypeer.rb
@@ -1,8 +1,8 @@
 class Sentrypeer < Formula
   desc "SIP honeypot for a distributed p2p list of bad actor IP addresses and phone numbers"
   homepage "https://sentrypeer.org"
-  url "https://github.com/SentryPeer/SentryPeer/releases/download/v0.0.4/sentrypeer-0.0.4.tar.gz"
-  sha256 "e9ab67001d6a49e808b8cc4cc0f8520ed75a2456be041a3c9b7c4543231751fb"
+  url "https://github.com/SentryPeer/SentryPeer/releases/download/v0.0.5/sentrypeer-0.0.5.tar.gz"
+  sha256 "db450f82aaf348ddb370937ead53f2df0a2d790b042a2c5c301c6e6f4847c67b"
   license any_of: ["GPL-2.0-only", "GPL-3.0-only"]
 
   depends_on "cmocka" => [:build, :test]
