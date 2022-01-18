b45ffcfa336467299a37740748964815bc6ba514
Gavin Henry <ghenry@surevoip.co.uk>
2022-01-18 21:33:40 +0000

    Correct sha256sum for v0.0.5.

--- sentrypeer.rb
+++ sentrypeer.rb
@@ -2,7 +2,7 @@ class Sentrypeer < Formula
   desc "SIP honeypot for a distributed p2p list of bad actor IP addresses and phone numbers"
   homepage "https://sentrypeer.org"
   url "https://github.com/SentryPeer/SentryPeer/releases/download/v0.0.5/sentrypeer-0.0.5.tar.gz"
-  sha256 "db450f82aaf348ddb370937ead53f2df0a2d790b042a2c5c301c6e6f4847c67b"
+  sha256 "bd0334cc71e17b53de8ef9200b39da752b1a8ec61ceb06fbaa61399b11075d12"
   license any_of: ["GPL-2.0-only", "GPL-3.0-only"]
 
   depends_on "cmocka" => [:build, :test]
