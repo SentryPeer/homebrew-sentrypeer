c6aa84cc6f13ac11510b73a12edae7f55db16b04
Gavin Henry <ghenry@surevoip.co.uk>
2022-02-23 10:03:21 +0000

    Clean up via "brew audit --new-formula"

--- sentrypeer.rb
+++ sentrypeer.rb
@@ -1,5 +1,5 @@
 class Sentrypeer < Formula
-  desc "SIP honeypot for a distributed p2p list of bad actor IP addresses and phone numbers"
+  desc "Honeypot for a distributed p2p list of bad actor IP addresses and phone numbers"
   homepage "https://sentrypeer.org"
   url "https://github.com/SentryPeer/SentryPeer/releases/download/v1.0.0/sentrypeer-1.0.0.tar.gz"
   sha256 "bfad4a572a3d7b17aa62b1642cc38b4ee786393068aeaa101ec3bcaf4ab970fd"
@@ -7,12 +7,12 @@ class Sentrypeer < Formula
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
-  depends_on "pkg-config" => :build
   depends_on "cmocka" => [:build, :test]
   depends_on "curl" => [:build, :test]
-  depends_on "libosip"
+  depends_on "pkg-config" => :build
   depends_on "jansson"
   depends_on "libmicrohttpd"
+  depends_on "libosip"
   depends_on "pcre2"
 
   def install
