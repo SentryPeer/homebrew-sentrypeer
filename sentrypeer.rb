3b8e31c531464f6039338db4b79992d5be7685ba
Gavin Henry <ghenry@surevoip.co.uk>
2022-09-01 19:01:39 +0100

    v2.0.0

--- sentrypeer.rb
+++ sentrypeer.rb
@@ -1,8 +1,8 @@
 class Sentrypeer < Formula
   desc "Honeypot for a distributed p2p list of bad actor IP addresses and phone numbers"
   homepage "https://sentrypeer.org"
-  url "https://github.com/SentryPeer/SentryPeer/releases/download/v1.4.1/sentrypeer-1.4.1.tar.gz"
-  sha256 "2d1fc0ab77823958bedd903c473c397bb4d43885ffd01ab6d04a1993aa5dd59f"
+  url "https://github.com/SentryPeer/SentryPeer/releases/download/v2.0.0/sentrypeer-2.0.0.tar.gz"
+  sha256 "da3c3e19b9c840d745baaa3b77d29706e86cfefc4519dee049020eabbab0b361"
   license any_of: ["GPL-2.0-only", "GPL-3.0-only"]
 
   depends_on "autoconf" => :build
@@ -15,6 +15,7 @@ class Sentrypeer < Formula
   depends_on "libosip"
   depends_on "pcre2"
   depends_on "opendht"
+  depends_on "curl"
 
   def install
     system "./configure", *std_configure_args, "--disable-silent-rules"
