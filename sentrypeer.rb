4014220de06de5e98fd983ffdbf4e3110bcb3758
Gavin Henry <ghenry@surevoip.co.uk>
2023-11-17 11:33:52 +0000

    Update sentrypeer.rb for v3.0.2

--- sentrypeer.rb
+++ sentrypeer.rb
@@ -1,8 +1,8 @@
 class Sentrypeer < Formula
   desc "Honeypot for a distributed p2p list of bad actor IP addresses and phone numbers"
   homepage "https://sentrypeer.org"
-  url "https://github.com/SentryPeer/SentryPeer/releases/download/v3.0.1/sentrypeer-3.0.1.tar.gz"
-  sha256 "7485ceaa06c58fcde6f9b0763988d5a405802ded218357991e898d91d821748f"
+  url "https://github.com/SentryPeer/SentryPeer/releases/download/v3.0.2/sentrypeer-3.0.2.tar.gz"
+  sha256 "10c21bcf7521949b0935124ed2f11b5d98f7879fc9e567c9a7ebd9cafbed8667"
   license any_of: ["GPL-2.0-only", "GPL-3.0-only"]
 
   depends_on "autoconf" => :build
