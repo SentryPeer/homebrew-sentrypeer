0cfe3f6bd6c4f7fd097096cdd452ddfde2beb830
Gavin Henry <ghenry@antnetworks.com>
2026-09-16 22:07:22 +0100

    Update to v4.0.5

--- sentrypeer.rb
+++ sentrypeer.rb
@@ -1,8 +1,8 @@
 class Sentrypeer < Formula
   desc "Honeypot for a distributed p2p list of bad actor IP addresses and phone numbers"
   homepage "https://sentrypeer.org"
-  url "https://github.com/SentryPeer/SentryPeer/releases/download/v4.0.4/sentrypeer-4.0.4.tar.gz"
-  sha256 "df30986e8fa48d2941a62b8ddb016cebc31f12f3e5af56df9b8b48e0bb451277"
+  url "https://github.com/SentryPeer/SentryPeer/releases/download/v4.0.5/sentrypeer-4.0.5.tar.gz"
+  sha256 "e7342207cf85c5ad5be7ed03b98efb28eb42849d74fa62f19690173ef301c2a3"
   license any_of: ["GPL-2.0-only", "GPL-3.0-only"]
 
   depends_on "autoconf" => :build
